757e7e1da4551c5614b421e01c8e0bb5b44c95cf
Dmitriy <dimonwes@gmail.com>
2021-10-30 13:15:11 +0300

    add quickSorting

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -4,11 +4,17 @@ import org.example.sorting.SortingUtils;
 import java.util.Arrays;
 
 public class Application {
+    static final int[] arrayForSort={5,3,7,2,6,9,8,1};
     public static void main(String[] args) {
         System.out.println("Hello word!");
-        int[] arrayForSort={5,3,7,2,6,9,8,1};
-        SortingUtils.sortingByChoice(arrayForSort);
-        System.out.println(Arrays.toString(arrayForSort));
+        //сортировка выбором
+        int[] forSortingByChoice=arrayForSort;
+        SortingUtils.sortingByChoice(forSortingByChoice);
+        System.out.println(Arrays.toString(forSortingByChoice));
+        //быстрая сортировка
+        forSortingByChoice=arrayForSort;
+        SortingUtils.quickSort(forSortingByChoice,0,forSortingByChoice.length-1);
+        System.out.println(Arrays.toString(forSortingByChoice));
 
     }
 }
--- src/main/java/org/example/sorting/SortingUtils.java
+++ src/main/java/org/example/sorting/SortingUtils.java
@@ -14,6 +14,37 @@ public class SortingUtils {
             swapInArray(array, leftIndex, minIndex);
         }
     }
+
+    public static void quickSort(int[] source, int leftBorder, int rightBorder) {
+        int leftMarker = leftBorder;
+        int rightPointer = rightBorder;
+        int centerElement = source[(leftMarker + rightPointer) / 2];
+        do {
+            while (source[leftMarker] < centerElement) {
+                leftMarker++;
+            }
+            while (source[rightPointer] > centerElement) {
+                rightPointer--;
+            }
+
+            if (leftMarker <= rightPointer) {
+                if (leftMarker < rightPointer) {
+                    swapInArray(source,leftMarker,rightPointer);
+                }
+                // сдвиг границ после выполненной операции
+                leftMarker++;
+                rightPointer--;
+            }
+        } while (leftMarker <= rightPointer);
+
+        if (leftMarker < rightBorder) {
+            quickSort(source, leftMarker, rightBorder);
+        }
+        if (leftBorder < rightPointer) {
+            quickSort(source, leftBorder, rightPointer);
+        }
+    }
+
     private  static void swapInArray(int[] array ,int index, int nextIndex){
 //      a= a xor b; b= b xor a; a= a xor b
 //      нельзя, если придут одинаковые значения, будут 0
